e90735de2508cdacf4f03b25cfd34cac0bd94062
Eyal Srugo <eyal.srugo@solaredge.com>
2021-10-28 16:32:03 +0300

    added chart tab (empty)

---
+++ Maxa Dash/Charts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace Maxa_Dash
+{
+    class Charts
+    {
+        
+    }
+}
